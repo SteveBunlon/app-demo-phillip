23506e328471282c8f15dcd8b2caf82a13740998
Steve Bunlon <bunlon.steve@gmail.com>
2021-07-26 12:15:02 +0200

    add forest integration

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/cars');
+const forest = require('forest-express-mongoose');
+const connection = require('./db');
+const models = connection.models;
 
-var app = express();
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/cars');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use('^(?!forest/?$).*', cors({
+  origin: ['https://app.development.forestadmin.com'],
+  credentials: true,
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,11 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/cars', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -36,6 +43,15 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+forest.init({
+  envSecret: process.env.FOREST_ENV_SECRET,
+  authSecret: process.env.FOREST_AUTH_SECRET,
+  objectMapping: models,
+  connections: { default: connection },
+}).then((FAApp) => {
+  app.use(FAApp);
 });
 
 module.exports = app;
